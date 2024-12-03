27bcb7bdeee5d3b46f6b4aab178982b05e3d5a6a
Jason Hammock <jhammock@protonmail.com>
2024-12-03 11:48:45 -0700

    day2 part1

--- bin/main.ml
+++ bin/main.ml
@@ -7,43 +7,68 @@ let read_file filename =
 let split_line line =
   String.split_on_chars ~on:[ ' ' ] line
   |> List.filter ~f:(fun s -> not (String.is_empty s))
+  |> List.map ~f:int_of_string
 ;;
 
-let update_map ~map ~key =
-  Map.update map key ~f:(function
-    | None -> 1
-    | Some value -> value + 1)
+type direction =
+  | Increasing
+  | Decreasing
+  | NotDetermined
+
+type trend_opt =
+  | Some of
+      { direction : direction
+      ; value : int
+      }
+  | None
+
+let determine_trend ~prev ~curr =
+  let diff = Int.abs (prev - curr) in
+  let valid = diff <= 3 && diff > 0 in
+  if prev > curr && valid
+  then Some { direction = Decreasing; value = curr }
+  else if prev < curr && valid
+  then Some { direction = Increasing; value = curr }
+  else None
 ;;
 
-let process_lines ~lines =
-  let c2_map = Map.empty (module Int) in
+let update_trend ~prev ~curr =
+  match prev, curr with
+  | Some { direction = NotDetermined; _ }, _ -> curr
+  | Some { direction = Increasing; _ }, Some { direction = Increasing; _ } -> curr
+  | Some { direction = Decreasing; _ }, Some { direction = Decreasing; _ } -> curr
+  | _ -> None
+;;
+
+let process_line line =
+  split_line line
+  |> List.fold
+       ~init:(Some { direction = NotDetermined; value = 0 })
+       ~f:(fun acc curr ->
+         match acc with
+         | None -> None
+         | Some { direction = NotDetermined; value = 0 } ->
+           Some { direction = NotDetermined; value = curr }
+         | Some prev ->
+           let current_trend = determine_trend ~prev:prev.value ~curr in
+           update_trend ~prev:acc ~curr:current_trend)
+;;
+
+let process_lines lines =
   List.fold
-    ~init:([], c2_map)
-    ~f:(fun (col1, map) s ->
-      match split_line s with
-      | [ c1; c2 ] -> int_of_string c1 :: col1, update_map ~map ~key:(int_of_string c2)
-      | _ -> failwith "invalid string")
+    ~init:0
+    ~f:(fun acc line ->
+      match process_line line with
+      | None -> acc
+      | Some _ -> acc + 1)
     lines
 ;;
 
-let similarity_score ~col1 ~map =
-  List.fold
-    ~init:0
-    ~f:(fun acc x ->
-      match Map.find map x with
-      | None -> acc
-      | Some y -> acc + (x * y))
-    col1
-;;
-
-let print_answer ~lines =
-  let col1, col2_map = process_lines ~lines in
-  print_endline (string_of_int (similarity_score ~col1 ~map:col2_map))
-;;
+let print_answer lines = print_endline (string_of_int (process_lines lines))
 
 let () =
-  let filename = "input/day1_2.txt" in
+  let filename = "input/day2.txt" in
   match read_file filename with
-  | lines -> print_answer ~lines
+  | lines -> print_answer lines
   | exception exn -> printf "Error: %s\n" (Exn.to_string exn)
 ;;
